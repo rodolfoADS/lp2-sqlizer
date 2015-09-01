5f3bc16ce98ad52e19e050d9c7948ecfef89285d
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2015-09-01 16:36:57 -0300

    Adicionado teste para consultas INSERT com valores double e Date

--- test/SQLInsertTest.java
+++ test/SQLInsertTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+
 import org.junit.Test;
 
 
@@ -26,6 +28,21 @@ public class SQLInsertTest {
 				.toString();
 		
 		assertEquals("INSERT INTO table (field, fieldB) VALUES (\"value\", 2);", sql);
+	}
+	
+	@Test
+	public void insertIntoSetDoubleSetDate() {
+		Calendar c = Calendar.getInstance();
+		c.set(2015, Calendar.SEPTEMBER, 1, 12, 0, 0);
+		
+		String sql = SQlizer
+				.insert()
+				.into("table")
+				.set("field", 2.55)
+				.set("fieldB", c.getTime())
+				.toString();
+		
+		assertEquals("INSERT INTO table (field, fieldB) VALUES (2.55, \"2015-09-01 12:00:00\");", sql);
 	}
 
 }
